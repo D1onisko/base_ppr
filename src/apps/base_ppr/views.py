c28c812cffd4116b38c558dea7d1ca5dc257040f
D1onisko <d1oniskozak@gmail.com>
2015-11-30 21:17:54 +0200

    подключение bootstrap 3.0,

--- src/apps/base_ppr/views.py
+++ src/apps/base_ppr/views.py
@@ -2,7 +2,6 @@
 from django.views.generic import DetailView, TemplateView, ListView
 from django.shortcuts import get_object_or_404, redirect
 
-from src.apps.base_ppr.models import TestIndex
 
 """
 Index page
