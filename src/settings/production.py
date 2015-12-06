29032edffa3d86fd63fcda59f41c92ff961d4f04
D1onisko <d1oniskozak@gmail.com>
2015-12-06 16:35:40 +0200

    tray 8

--- src/settings/production.py
+++ src/settings/production.py
@@ -7,16 +7,6 @@ DEBUG = False
 TEMPLATE_DEBUG = True
 DATABASES = settings.DATABASES
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -27,3 +17,13 @@ conn = psycopg2.connect(
     host=url.hostname,
     port=url.port
 )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
